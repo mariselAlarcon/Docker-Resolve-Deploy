6d9acd96a1bb420ac77499f82d912210a4426453
MaxMartinezX <maxipro328@gmail.com>
2024-11-25 21:05:07 -0300

    cambios de maxi en clases
    lol

--- mongo_backups/backup.sh
+++ mongo_backups/backup.sh
@@ -8,7 +8,13 @@ sleep 10
 
 while true; do
   TIMESTAMP=$(date '+%Y-%m-%d_%H-%M-%S')
-  mongodump --host=mongo_db --port=27017 --db=gym_db --username=admin --password=admin1234 --authenticationDatabase=admin --authenticationMechanism=SCRAM-SHA-1 --archive=$BACKUP_DIR/backup_$TIMESTAMP.gz
+  mongodump --host="mongoReplicaSet/mongo_db:27017,mongo_db1:27017,mongo_db2:27017" \
+  --db=gym_db \
+  --username=admin \
+  --password=admin1234 \
+  --authenticationDatabase=admin \
+  --authenticationMechanism=SCRAM-SHA-1 \
+  --archive=$BACKUP_DIR/backup_$TIMESTAMP.gz
   echo "Backup creado en $BACKUP_DIR/backup_$TIMESTAMP.gz"
-  sleep 1800
+  sleep 60
 done
